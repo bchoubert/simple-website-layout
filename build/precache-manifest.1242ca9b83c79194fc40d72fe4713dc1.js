3f94ff5511e9faad144f3f59f002e03e011dfc59
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2019-11-30 14:58:26 +0100

    Added build

---
+++ build/precache-manifest.1242ca9b83c79194fc40d72fe4713dc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f033360898cb5dfa25e742d52dc954e1",
+    "url": "/simple-website-layout/build/index.html"
+  },
+  {
+    "revision": "8f1057bad0f0411a1dd2",
+    "url": "/simple-website-layout/build/static/css/main.9dabd53f.chunk.css"
+  },
+  {
+    "revision": "e5e853f42676a278a264",
+    "url": "/simple-website-layout/build/static/js/2.1e63dcd8.chunk.js"
+  },
+  {
+    "revision": "8f1057bad0f0411a1dd2",
+    "url": "/simple-website-layout/build/static/js/main.aaa0d77c.chunk.js"
+  },
+  {
+    "revision": "604991c288950fca0db3",
+    "url": "/simple-website-layout/build/static/js/runtime-main.a28a55ab.js"
+  }
+]);
